9846aa1dbd8a301be06cd2e06a2f6958c3e0418c
Pomichal <vajk.pomichal@gmail.com>
2021-04-13 17:07:39 +0200

    Add GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Awake()
+    {
+        App.gameManager = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
