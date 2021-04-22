e3a5d8c4c176df978bcb7bd0ccf2b5a050eb66f1
Pomichal <vajk.pomichal@gmail.com>
2021-04-22 19:53:28 +0200

    Added camera follow

---
+++ Assets/Scripts/Behaviours/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    [Range(0, 1f)]
+    public float t;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), t);
+    }
+}
