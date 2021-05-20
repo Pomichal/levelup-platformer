3adadb5ffb4d2f9c571fe9287e94b580ef1fb069
Pomichal <vajk.pomichal@gmail.com>
2021-05-20 18:11:24 +0200

    Switch to URP & eyes following the player

---
+++ Assets/Scripts/Behaviours/EyeBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeBehaviour : MonoBehaviour
+{
+
+    public float offset;
+    public Transform eye;
+
+    // Update is called once per frame
+    void Update()
+    {
+        eye.position = transform.position + (App.playersTransform.position - transform.position).normalized * offset;
+    }
+}
