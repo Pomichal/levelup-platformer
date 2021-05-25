b6c6dce6e88fbfdc87320365a2fcbd9920960601
Pomichal <vajk.pomichal@gmail.com>
2021-05-25 18:15:54 +0200

    ADD: shader examples

---
+++ Assets/Scripts/Behaviours/DissolveBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveBehaviour : MonoBehaviour
+{
+
+    Material mat;
+    float timer;
+
+    void Awake()
+    {
+        mat = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            timer = 3f;
+            mat.SetFloat("Fade", 0);
+        }
+
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            mat.SetFloat("Fade", timer / 3f);
+        }
+    }
+}
