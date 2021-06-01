7d26f9c3bf0f9ab9b6f555ac5f6b409e9f51425e
Pomichal <vajk.pomichal@gmail.com>
2021-06-01 18:00:11 +0200

    Added outline shader and basic wind shader

---
+++ Assets/Scripts/Behaviours/TreeBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBehaviour : MonoBehaviour
+{
+
+    public float scale;
+    Material mat;
+
+    void Awake()
+    {
+        mat = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.SetFloat("_OutlineWidth", Mathf.Abs(Mathf.Sin(Time.time)) * scale);
+    }
+}
